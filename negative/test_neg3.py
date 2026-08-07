c29d3b91681772cfac8121d739feea44e69855c2
Irina <irrina.s@gmail.com>
2026-08-08 04:37:36 +0900

    les8

---
+++ negative/test_neg3.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def test_create_project():
+    token = os.getenv("auth_secret_0c2k")
+    update_data = {"title": "New"}
+    id = "d0b2c270-37e4-4040-8562-0040bdYYY592"
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
+    response = requests.put(url=f"https://ru.yougile.com/api-v2/projects/{id}",
+                            json=update_data, headers=headers)
+    print(response.text)
+    assert response.status_code == 404
---
+++ positive/test_get.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def test_get_project():
+    token = os.getenv("auth_secret_0c2k")
+    id = "d0b2c270-37e4-4040-8562-0040bd097592"
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
+    response = requests.get(
+        url=f"https://ru.yougile.com/api-v2/projects/{id}", headers=headers)
+    print(response.text)
+    assert response.status_code == 200
